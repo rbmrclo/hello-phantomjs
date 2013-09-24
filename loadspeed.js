c4faad4cd684f3550b0de6d07bae19316cb0d542
Robbie Marcelo <rbmrclo@hotmail.com>
2013-09-24 13:47:29 +0800

    Phantomjs :ghost:

---
+++ loadspeed.js
@@ -0,0 +1,25 @@
+var page = require('webpage').create(),
+    system = require('system'),
+    t, address;
+
+if (system.args.length === 1) {
+    console.log('Usage: loadspeed.js <some URL>');
+    phantom.exit(1);
+} else {
+    t = Date.now();
+    address = system.args[1];
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+        } else {
+            t = Date.now() - t;
+            console.log('Page title is ' + page.evaluate(function () {
+                return document.title;
+            }));
+            console.log('Loading time ' + t + ' msec');
+        }
+        phantom.exit();
+    });
+}
+
+// panthomjs loadspeed.js http://any-url-here.com
